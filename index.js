787a675b3fc4ff9e2e1af59e3c22770d927759bf
ktr0328 <zf7heatpa@gmail.com>
2017-05-23 16:59:23 +0900

    pugの読み込み
    テスト段階
    index.pugを読んでhtmlの生成をする
    あとはうんたらかんたら

--- index.js
+++ index.js
@@ -1,16 +1,24 @@
+'use strict';
+
 const electron = require('electron');
 const app = electron.app;
 const BrowserWindow = electron.BrowserWindow;
 
+const locals = {/* ...*/};
+const pug = require('electron-pug')({pretty: true}, locals);
+
 let mainWindow = null;
 app.on('ready', () => {
     // mainWindowを作成（windowの大きさや、Kioskモードにするかどうかなどもここで定義できる）
-    mainWindow = new BrowserWindow({width: 400, height: 300});
+    mainWindow = new BrowserWindow({
+            width: 800,
+            height: 1000,
+        }
+    );
 // Electronに表示するhtmlを絶対パスで指定（相対パスだと動かない）
-    mainWindow.loadURL('file://' + __dirname + '/index.html');
+    mainWindow.loadURL('file://' + __dirname + '/index.pug');
 
-// ChromiumのDevツールを開く
-    mainWindow.webContents.openDevTools();
+    // mainWindow.webContents.openDevTools();
 
     mainWindow.on('closed', function () {
         mainWindow = null;
