952abc417ae6d4bb059fd95e8e865877bb0a0cce
ktr0328 <zf7heatpa@gmail.com>
2017-05-24 03:33:38 +0900

    jQuery導入
    にあたってbuildが必要になった
    npm run buildで起動できる
    
    詳細
    browserifyを使ってjQueryをbundleに突っ込む
    何かまた必要なものが増えれば:buildを付けてscriptsに追加すべし

---
+++ dest/script.js
@@ -0,0 +1,9 @@
+/**
+ * Created by zf7he on 2017/05/24.
+ */
+const $ = require('jquery');
+
+$(function () {
+    console.log("あああああ");
+    // $('#mainPanel').css('background-color', 'black');
+});
